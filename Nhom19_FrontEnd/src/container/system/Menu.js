32a2d445b5ba483f4cb13839f4cb17cf8df15a2e
Your Name <Hongson16042003@gmail.com>
2025-04-03 15:18:22 +0700

    Coding FrontEnd Quan Ly Nguoi Dung

--- Nhom19_FrontEnd/src/container/system/Menu.js
+++ Nhom19_FrontEnd/src/container/system/Menu.js
@@ -3,47 +3,88 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const Menu = () => {
-    const [user, setUser] = useState({});
-    useEffect(() => {
-        const userData = JSON.parse(localStorage.getItem("userData"));
-        setUser(userData);
-    }, []);
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    setUser(userData);
+  }, []);
 
-    return (
-        <nav
-            style={{ backgroundColor: "#e2ebf8", width: "278px" }}
-            className="sidebar sidebar-offcanvas"
-            id="sidebar"
-        >
-            <ul className="nav">
-                {user && user.roleCode === "ADMIN" && (
-                    <li className="nav-item relative">
-                        <a
-                            className="nav-link"
-                            data-toggle="collapse"
-                            href="#admin"
-                            aria-expanded="false"
-                            aria-controls="auth"
-                        >
-                            <Link className="nav-link_active" to="/admin/">
-                                <i className="icon-grid menu-icon" />
-                                <span
-                                    style={{
-                                        fontSize: "16px",
-                                        marginTop: "5px",
-                                    }}
-                                    className="menu-title"
-                                >
-                                    Trang chủ
-                                </span>
-                            </Link>
-                        </a>
-                        <div className="collapse" id="admin"></div>
-                    </li>
-                )}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav
+      style={{ backgroundColor: "#e2ebf8", width: "278px" }}
+      className="sidebar sidebar-offcanvas"
+      id="sidebar"
+    >
+      <ul className="nav">
+        {user && user.roleCode === "ADMIN" && (
+          <li className="nav-item relative">
+            <a
+              className="nav-link"
+              data-toggle="collapse"
+              href="#admin"
+              aria-expanded="false"
+              aria-controls="auth"
+            >
+              <Link className="nav-link_active" to="/admin/">
+                <i className="icon-grid menu-icon" />
+                <span
+                  style={{
+                    fontSize: "16px",
+                    marginTop: "5px",
+                  }}
+                  className="menu-title"
+                >
+                  Trang chủ
+                </span>
+              </Link>
+            </a>
+            <div className="collapse" id="admin"></div>
+          </li>
+        )}
+        {
+          <li className="nav-item relative">
+            <a
+              className="nav-link"
+              data-toggle="collapse"
+              href="#auth"
+              aria-expanded="false"
+              aria-controls="auth"
+            >
+              <i className="icon-head menu-icon" />
+              <Link className="nav-link_active" to="/admin/list-user/">
+                <span
+                  style={{ fontSize: "16px", marginTop: "4px" }}
+                  className="menu-title"
+                >
+                  Quản lý người dùng
+                </span>
+              </Link>
+              {/* <i className="menu-arrow" /> */}
+            </a>
+            <div className="collapse" id="auth">
+              {/* <ul className="nav flex-column sub-menu">
+                        <li className="nav-item relative">
+                          {" "}
+                          <Link className="nav-link" to="/admin/list-user/">
+                            {" "}
+                            Danh sách người dùng{" "}
+                          </Link>
+                        </li>
+                        <li className="nav-item relative">
+                          {" "}
+                          <Link className="nav-link" to="/admin/add-user/">
+                            {" "}
+                            Thêm người dùng{" "}
+                          </Link>
+                        </li>
+                      </ul> */}
+            </div>
+          </li>
+          
+        }
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
