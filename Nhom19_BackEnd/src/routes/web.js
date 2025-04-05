3027613fbde3d7672c519af3c4f37d6d95c0eadd
Phuoctai31023 <phuoctai31023@gmail.com>
2025-04-05 17:19:45 +0700

    Coding BackEnd chuc nang Quan ly ky nang

--- Nhom19_BackEnd/src/routes/web.js
+++ Nhom19_BackEnd/src/routes/web.js
@@ -1,37 +1,119 @@
 import express from "express";
-import userController from '../controllers/userController';
-import companyController from '../controllers/companyController';
-import middlewareControllers from '../middlewares/jwtVerify'
+import userController from "../controllers/userController";
+import companyController from "../controllers/companyController";
+import middlewareControllers from "../middlewares/jwtVerify";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
+  //=====================API USER==========================//
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/update-user", userController.handleUpdateUser);
+  router.post(
+    "/api/ban-user",
+    middlewareControllers.verifyTokenAdmin,
+    userController.handleBanUser
+  );
+  router.post(
+    "/api/unban-user",
+    middlewareControllers.verifyTokenAdmin,
+    userController.handleUnbanUser
+  );
+  router.post("/api/login", userController.handleLogin);
+  router.get(
+    "/api/get-all-user",
+    middlewareControllers.verifyTokenUser,
+    userController.getAllUser
+  );
+  router.get(
+    "/api/get-detail-user-by-id",
+    middlewareControllers.verifyTokenUser,
+    userController.getDetailUserById
+  );
 
-    //=====================API USER==========================//
-    router.post('/api/create-new-user', userController.handleCreateNewUser)
-    router.put('/api/update-user', userController.handleUpdateUser)
-    router.post('/api/ban-user', middlewareControllers.verifyTokenAdmin ,userController.handleBanUser)
-    router.post('/api/unban-user', middlewareControllers.verifyTokenAdmin ,userController.handleUnbanUser)
-    router.post('/api/login', userController.handleLogin)
-    router.get('/api/get-all-user', middlewareControllers.verifyTokenUser,userController.getAllUser)
-    router.get('/api/get-detail-user-by-id', middlewareControllers.verifyTokenUser,userController.getDetailUserById)
+  //===================API ALLCODE========================//
+  router.post(
+    "/api/create-new-all-code",
+    middlewareControllers.verifyTokenAdmin,
+    allcodeController.handleCreateNewAllCode
+  );
+  router.put(
+    "/api/update-all-code",
+    middlewareControllers.verifyTokenAdmin,
+    allcodeController.handleUpdateAllCode
+  );
+  router.delete(
+    "/api/delete-all-code",
+    middlewareControllers.verifyTokenAdmin,
+    allcodeController.handleDeleteAllCode
+  );
+  router.get("/api/get-all-code", allcodeController.getAllCodeService);
+  router.get("/api/get-list-allcode", allcodeController.getListAllCodeService);
+  router.get(
+    "/api/get-detail-all-code-by-code",
+    allcodeController.getDetailAllcodeByCode
+  );
+  router.get(
+    "/api/get-list-job-count-post",
+    allcodeController.getListJobTypeAndCountPost
+  );
 
-     //===================API ALLCODE========================//
-     router.post('/api/create-new-all-code',middlewareControllers.verifyTokenAdmin ,allcodeController.handleCreateNewAllCode)
-     router.put('/api/update-all-code', middlewareControllers.verifyTokenAdmin,allcodeController.handleUpdateAllCode)
-     router.delete('/api/delete-all-code', middlewareControllers.verifyTokenAdmin,allcodeController.handleDeleteAllCode)
-     router.get('/api/get-all-code', allcodeController.getAllCodeService)
-     router.get('/api/get-list-allcode', allcodeController.getListAllCodeService)
-     router.get('/api/get-detail-all-code-by-code', allcodeController.getDetailAllcodeByCode)
-     router.get('/api/get-list-job-count-post', allcodeController.getListJobTypeAndCountPost)
+  //==================API COMPANY=========================//
+  router.post(
+    "/api/create-new-company",
+    middlewareControllers.verifyTokenUser,
+    companyController.handleCreateNewCompany
+  );
+  router.put(
+    "/api/update-company",
+    middlewareControllers.verifyTokenUser,
+    companyController.handleUpdateCompany
+  );
+  router.put(
+    "/api/ban-company",
+    middlewareControllers.verifyTokenAdmin,
+    companyController.handleBanCompany
+  );
+  router.put(
+    "/api/unban-company",
+    middlewareControllers.verifyTokenAdmin,
+    companyController.handleUnBanCompany
+  );
+  router.put(
+    "/api/add-user-company",
+    middlewareControllers.verifyTokenUser,
+    companyController.handleAddUserCompany
+  );
+  router.get("/api/get-list-company", companyController.getListCompany);
+  router.get(
+    "/api/get-detail-company-by-id",
+    companyController.getDetailCompanyById
+  );
+  router.get(
+    "/api/get-detail-company-by-userId",
+    companyController.getDetailCompanyByUserId
+  );
+  router.get(
+    "/api/get-all-user-by-companyId",
+    middlewareControllers.verifyTokenUser,
+    companyController.getAllUserByCompanyId
+  );
+  router.put(
+    "/api/quit-company",
+    middlewareControllers.verifyTokenUser,
+    companyController.handleQuitCompany
+  );
+  router.get(
+    "/api/get-all-company",
+    middlewareControllers.verifyTokenAdmin,
+    companyController.getAllCompanyByAdmin
+  );
+  router.put(
+    "/api/accecpt-company",
+    middlewareControllers.verifyTokenAdmin,
+    companyController.handleAccecptCompany
+  );
 
-     
-    //==================API COMPANY=========================//
-    router.post('/api/create-new-company', middlewareControllers.verifyTokenUser,companyController.handleCreateNewCompany)
-    router.put('/api/update-company', middlewareControllers.verifyTokenUser,companyController.handleUpdateCompany)
-    router.get('/api/get-detail-company-by-id', companyController.getDetailCompanyById)
-    router.get('/api/get-detail-company-by-userId',companyController.getDetailCompanyByUserId)
-
-    return app.use("/", router);
-}
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
--- Nhom19_FrontEnd/src/service/userService.js
+++ Nhom19_FrontEnd/src/service/userService.js
@@ -1,143 +1,223 @@
 import axios from "../axios";
 
-
 //==================USER==========================//
 const getAllUsers = (data) => {
-    return axios.get(`/api/get-all-user?limit=${data.limit}&offset=${data.offset}&search=${data.search}`)
-
-}
+  return axios.get(
+    `/api/get-all-user?limit=${data.limit}&offset=${data.offset}&search=${data.search}`
+  );
+};
 const createNewUser = (data) => {
-    return axios.post(`/api/create-new-user`, data)
-
-}
+  return axios.post(`/api/create-new-user`, data);
+};
 const UpdateUserService = (data) => {
-    return axios.put(`/api/update-user`, data)
-
-}
+  return axios.put(`/api/update-user`, data);
+};
 const BanUserService = (userId) => {
-    return axios.post(`/api/ban-user`, {
-        data: {
-            id: userId
-        }
-    })
-
-}
+  return axios.post(`/api/ban-user`, {
+    data: {
+      id: userId,
+    },
+  });
+};
 
 const UnbanUserService = (userId) => {
-    return axios.post(`/api/unban-user`, {
-        data: {
-            id: userId
-        }
-    })
-
-}
-
+  return axios.post(`/api/unban-user`, {
+    data: {
+      id: userId,
+    },
+  });
+};
 
 const getDetailUserById = (id) => {
-    return axios.get(`/api/get-detail-user-by-id?id=${id}`)
-
-}
+  return axios.get(`/api/get-detail-user-by-id?id=${id}`);
+};
 const handleLoginService = (data) => {
-    return axios.post(`/api/login`, data)
-
-}
+  return axios.post(`/api/login`, data);
+};
 
 //===============ALL CODE========================//
 const getAllCodeService = (type) => {
-    return axios.get(`/api/get-all-code?type=${type}`)
-
-}
+  return axios.get(`/api/get-all-code?type=${type}`);
+};
 const getListAllCodeService = (data) => {
-    return axios.get(`/api/get-list-allcode?type=${data.type}&limit=${data.limit}&offset=${data.offset}&search=${data.search}`)
-
-}
+  return axios.get(
+    `/api/get-list-allcode?type=${data.type}&limit=${data.limit}&offset=${data.offset}&search=${data.search}`
+  );
+};
 
 const getListJobTypeAndCountPost = (data) => {
-    return axios.get(`/api/get-list-job-count-post?limit=${data.limit}&offset=${data.offset}`)
-
-}
+  return axios.get(
+    `/api/get-list-job-count-post?limit=${data.limit}&offset=${data.offset}`
+  );
+};
 
 const createAllCodeService = (data) => {
-    return axios.post(`/api/create-new-all-code`, data)
-
-}
+  return axios.post(`/api/create-new-all-code`, data);
+};
 
 const getDetailAllcodeByCode = (code) => {
-    return axios.get(`/api/get-detail-all-code-by-code?code=${code}`)
-
-}
+  return axios.get(`/api/get-detail-all-code-by-code?code=${code}`);
+};
 const UpdateAllcodeService = (data) => {
-    return axios.put(`/api/update-all-code`, data)
-
-}
+  return axios.put(`/api/update-all-code`, data);
+};
 const DeleteAllcodeService = (allcodeId) => {
-    return axios.delete(`/api/delete-all-code`, {
-        data: {
-            code: allcodeId
-        }
-    })
-}
+  return axios.delete(`/api/delete-all-code`, {
+    data: {
+      code: allcodeId,
+    },
+  });
+};
 
 const getListSkill = (data) => {
-    return axios.get(`/api/get-list-skill?categoryJobCode=${data.categoryJobCode}&limit=${data.limit}&offset=${data.offset}&search=${data.search}`)
-
-}
+  return axios.get(
+    `/api/get-list-skill?categoryJobCode=${data.categoryJobCode}&limit=${data.limit}&offset=${data.offset}&search=${data.search}`
+  );
+};
 
 const getAllSkillByJobCode = (categoryJobCode) => {
-    return axios.get(`/api/get-all-skill-by-job-code?categoryJobCode=${categoryJobCode}`)
-
-}
+  return axios.get(
+    `/api/get-all-skill-by-job-code?categoryJobCode=${categoryJobCode}`
+  );
+};
 
 const createSkilleService = (data) => {
-    return axios.post(`/api/create-new-skill`, data)
-
-}
+  return axios.post(`/api/create-new-skill`, data);
+};
 
 const UpdateSkillService = (data) => {
-    return axios.put(`/api/update-skill`, data)
-
-}
+  return axios.put(`/api/update-skill`, data);
+};
 const DeleteSkillService = (skillId) => {
-    return axios.delete(`/api/delete-skill`, {
-        data: {
-            id: skillId
-        }
-    })
-}
+  return axios.delete(`/api/delete-skill`, {
+    data: {
+      id: skillId,
+    },
+  });
+};
 
 const getDetailSkillById = (id) => {
-    return axios.get(`/api/get-detail-skill-by-id?id=${id}`)
-
-}
+  return axios.get(`/api/get-detail-skill-by-id?id=${id}`);
+};
 
 //================================== COMPANY ============================
 const createCompanyService = (data) => {
-    return axios.post(`/api/create-new-company`, data)
-
-}
-const getDetailCompanyByUserId = (userId,companyId) => {
-    return axios.get(`/api/get-detail-company-by-userId?userId=${userId}&companyId=${companyId}`)
-
-}
+  return axios.post(`/api/create-new-company`, data);
+};
+const getDetailCompanyByUserId = (userId, companyId) => {
+  return axios.get(
+    `/api/get-detail-company-by-userId?userId=${userId}&companyId=${companyId}`
+  );
+};
 const getDetailCompanyById = (id) => {
-    return axios.get(`/api/get-detail-company-by-id?id=${id}`)
-
-}
+  return axios.get(`/api/get-detail-company-by-id?id=${id}`);
+};
 const updateCompanyService = (data) => {
-    return axios.put(`/api/update-company`, data)
+  return axios.put(`/api/update-company`, data);
+};
+const RecruitmentService = (data) => {
+  return axios.put(`/api/add-user-company`, data);
+};
+const getAllUserByCompanyIdService = (data) => {
+  return axios.get(
+    `/api/get-all-user-by-companyId?companyId=${data.companyId}&limit=${data.limit}&offset=${data.offset}`
+  );
+};
+const QuitCompanyService = (data) => {
+  return axios.put(`/api/quit-company`, data);
+};
+const getListCompany = (data) => {
+  return axios.get(
+    `/api/get-list-company?limit=${data.limit}&offset=${data.offset}&search=${data.search}`
+  );
+};
 
-}
+const getAllCompany = (data) => {
+  return axios.get(
+    `/api/get-all-company?limit=${data.limit}&offset=${data.offset}&search=${data.search}&censorCode=${data.censorCode}`
+  );
+};
 
+const banCompanyService = (data) => {
+  return axios.put(`/api/ban-company`, data);
+};
+
+const unbanCompanyService = (data) => {
+  return axios.put(`/api/unban-company`, data);
+};
+
+const accecptCompanyService = (data) => {
+  return axios.put(`/api/accecpt-company`, data);
+};
 export {
-    DeleteAllcodeService, UpdateAllcodeService, getDetailAllcodeByCode, createAllCodeService, getListAllCodeService, getAllCodeService,
-    getAllUsers, createNewUser, UpdateUserService, BanUserService,UnbanUserService, getDetailUserById, handleChangePassword, handleLoginService,
-    createCompanyService, getDetailCompanyByUserId, updateCompanyService, RecruitmentService, getAllUserByCompanyIdService, QuitCompanyService,
-    createPostService, updatePostService, banPostService,acceptPostService, getAllPostByAdminService,getAllPostByRoleAdminService, getDetailPostByIdService, activePostService, checkUserPhoneService, getListPostService,
-    getListJobTypeAndCountPost, getListCompany, getDetailCompanyById,changePasswordByphone,getStatisticalTypePost , getPackageByType, getPaymentLink , paymentOrderSuccessService , getAllPackage ,
-    setActiveTypePackage , createPackagePost , getPackageById , updatePackagePost , getStatisticalPackagePost,
-    getListNoteByPost , getAllCompany , accecptCompanyService, reupPostService, banCompanyService, unbanCompanyService,
-    getListSkill, getAllSkillByJobCode, createSkilleService, UpdateSkillService, DeleteSkillService, getDetailSkillById,
-    UpdateUserSettingService,
-    getPackageByIdCv, getAllPackageCv, getPaymentLinkCv, paymentOrderSuccessServiceCv, setActiveTypePackageCv, createPackageCv, updatePackageCv,getStatisticalPackageCv, getAllToSelect,
-    getHistoryTradeCv, getHistoryTradePost, getSumByYearCv, getSumByYearPost
-}
+  DeleteAllcodeService,
+  UpdateAllcodeService,
+  getDetailAllcodeByCode,
+  createAllCodeService,
+  getListAllCodeService,
+  getAllCodeService,
+  getAllUsers,
+  createNewUser,
+  UpdateUserService,
+  BanUserService,
+  UnbanUserService,
+  getDetailUserById,
+  handleChangePassword,
+  handleLoginService,
+  createCompanyService,
+  getDetailCompanyByUserId,
+  updateCompanyService,
+  RecruitmentService,
+  getAllUserByCompanyIdService,
+  QuitCompanyService,
+  createPostService,
+  updatePostService,
+  banPostService,
+  acceptPostService,
+  getAllPostByAdminService,
+  getAllPostByRoleAdminService,
+  getDetailPostByIdService,
+  activePostService,
+  checkUserPhoneService,
+  getListPostService,
+  getListJobTypeAndCountPost,
+  getListCompany,
+  getDetailCompanyById,
+  changePasswordByphone,
+  getStatisticalTypePost,
+  getPackageByType,
+  getPaymentLink,
+  paymentOrderSuccessService,
+  getAllPackage,
+  setActiveTypePackage,
+  createPackagePost,
+  getPackageById,
+  updatePackagePost,
+  getStatisticalPackagePost,
+  getListNoteByPost,
+  getAllCompany,
+  accecptCompanyService,
+  reupPostService,
+  banCompanyService,
+  unbanCompanyService,
+  getListSkill,
+  getAllSkillByJobCode,
+  createSkilleService,
+  UpdateSkillService,
+  DeleteSkillService,
+  getDetailSkillById,
+  UpdateUserSettingService,
+  getPackageByIdCv,
+  getAllPackageCv,
+  getPaymentLinkCv,
+  paymentOrderSuccessServiceCv,
+  setActiveTypePackageCv,
+  createPackageCv,
+  updatePackageCv,
+  getStatisticalPackageCv,
+  getAllToSelect,
+  getHistoryTradeCv,
+  getHistoryTradePost,
+  getSumByYearCv,
+  getSumByYearPost,
+};
