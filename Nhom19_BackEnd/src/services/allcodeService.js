0b415e57f6dc7cf7601775a8b45da53269f3f453
sang1003 <mem830740@gmail.com>
2025-04-06 17:48:38 +0700

    Coding BackEnd chức năng Quan ly khoang luong

--- Nhom19_BackEnd/src/services/allcodeService.js
+++ Nhom19_BackEnd/src/services/allcodeService.js
@@ -1,4 +1,255 @@
 import db from "../models/index";
+
+let handleCreateNewSkill = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.name || !data.categoryJobCode) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters !'
+                })
+            } else {
+
+                let res = await db.Skill.findOne({
+                    where: { categoryJobCode: data.categoryJobCode, name: data.name }
+                })
+
+                if (res) {
+                    resolve({
+                        errCode: 2,
+                        errMessage: 'Tên kĩ năng đã có trong lĩnh vực này !'
+                    })
+                } else {
+                    await db.Skill.create({
+                        name: data.name,
+                        categoryJobCode: data.categoryJobCode
+                    })
+                    resolve({
+                        errCode: 0,
+                        errMessage: 'ok'
+                    })
+                }
+
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let handleDeleteSkill = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMessage: `Missing required parameters !`
+                })
+            } else {
+                let foundSkill = await db.Skill.findOne({
+                    where: { id: id }
+                })
+                if (!foundSkill) {
+                    resolve({
+                        errCode: 2,
+                        errMessage: `Không tồn tại kĩ năng`
+                    })
+                }
+                else {
+                    let isSkillUsed = await db.UserSkill.findOne({
+                        where: { skillId: id }
+                    })
+                    if (isSkillUsed) {
+                        resolve({
+                            errCode: 3,
+                            errMessage: `Bạn không thể xóa thông tin này vì các dữ liệu khác liên quan`
+                        })
+                    }
+                    else {
+                        await db.Skill.destroy({
+                            where: { id: id }
+                        })
+                    }
+                    resolve({
+                        errCode: 0,
+                        errMessage: `Đã xóa thành công`
+                    })
+                }
+            }
+
+        } catch (error) {
+            reject(error.message)
+        }
+    })
+}
+
+let getAllSkillByJobCode = (categoryJobCode) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!categoryJobCode) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters !'
+                })
+            }
+            else {
+                let objectFilter = {
+                    include: [
+                        { model: db.Allcode, as: 'jobTypeSkillData', attributes: ['value', 'code'] }
+                    ],
+                    raw: true,
+                    nest: true
+                }
+                if (categoryJobCode !== 'getAll') {
+                    objectFilter.where = {
+                        [Op.and]: [
+                            db.Sequelize.where(db.sequelize.col('jobTypeSkillData.code'),{
+                                [Op.eq]: categoryJobCode
+                            }),
+                        ]
+                    }
+                }
+                let skills = await db.Skill.findAll(objectFilter)
+                resolve({
+                    errCode: 0,
+                    data: skills
+                })
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let getListSkill = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.limit || !data.offset) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters !'
+                })
+            }
+            else {
+                let objectFilter = {
+                    include: [
+                        { model: db.Allcode, as: 'jobTypeSkillData', attributes: ['value', 'code'] }
+                    ],
+                    limit: +data.limit,
+                    offset: +data.offset,
+                    raw: true,
+                    nest: true
+                }
+                if (data.search) {
+                    objectFilter.where = {
+                        ...objectFilter.where,
+                        name: {
+                            [Op.like]: `%${data.search}%`
+                        }
+                    }
+                }
+                if (data.categoryJobCode) {
+                    objectFilter.where = {
+                        ...objectFilter.where,
+                        [Op.and]: [
+                                db.Sequelize.where(db.sequelize.col('jobTypeSkillData.code'),{
+                                    [Op.eq]: data.categoryJobCode
+                                }),
+                            ]
+                    }
+                }
+                let skills = await db.Skill.findAndCountAll(objectFilter)
+                resolve({
+                    errCode: 0,
+                    data: skills.rows,
+                    count: skills.count
+                })
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let handleUpdateSkill = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.name || !data.id || !data.categoryJobCode) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters !'
+                })
+            } else {
+                let res = await db.Skill.findOne({
+                    where: {
+                        id: data.id
+                    },
+                    raw: false
+                })
+                if (res) {
+                    res.name = data.name
+                    res.categoryJobCode = data.categoryJobCode
+                    res = await res.save();
+                    if (res)
+                        resolve({
+                            errCode: 0,
+                            errMessage: 'Đã chỉnh sửa thành công'
+                        })
+                    else {
+                        resolve({
+                            errCode: 1,
+                            errMessage: 'Có lỗi trong quá trình chỉnh sửa'
+                        })
+                    }
+                }
+                else {
+                    resolve({
+                        errCode: 2,
+                        errMessage: 'Không tồn tại kĩ năng'
+                    })
+                }
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let getDetailSkillById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters !'
+                })
+            } else {
+                let data = await db.Skill.findOne({
+                    where: { id: id },
+                    include: [
+                        {model: db.Allcode, as: 'jobTypeSkillData', attributes: ['value','code']}
+                    ],
+                    nest: true,
+                    raw: true
+                })
+                if (data)
+                    resolve({
+                        errCode: 0,
+                        data: data
+                    })
+                else {
+                    resolve({
+                        errCode: 1,
+                        errMessage: 'Không tìm thấy code'
+                    })
+                }
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 let handleCreateNewAllCode = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -255,265 +506,17 @@ let getListJobTypeAndCountPost = async (data) => {
     })
 }
 
-let handleCreateNewSkill = (data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!data.name || !data.categoryJobCode) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing required parameters !'
-                })
-            } else {
-
-                let res = await db.Skill.findOne({
-                    where: { categoryJobCode: data.categoryJobCode, name: data.name }
-                })
-
-                if (res) {
-                    resolve({
-                        errCode: 2,
-                        errMessage: 'Tên kĩ năng đã có trong lĩnh vực này !'
-                    })
-                } else {
-                    await db.Skill.create({
-                        name: data.name,
-                        categoryJobCode: data.categoryJobCode
-                    })
-                    resolve({
-                        errCode: 0,
-                        errMessage: 'ok'
-                    })
-                }
-
-            }
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
-
-let handleDeleteSkill = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-
-            if (!id) {
-                resolve({
-                    errCode: 1,
-                    errMessage: `Missing required parameters !`
-                })
-            } else {
-                let foundSkill = await db.Skill.findOne({
-                    where: { id: id }
-                })
-                if (!foundSkill) {
-                    resolve({
-                        errCode: 2,
-                        errMessage: `Không tồn tại kĩ năng`
-                    })
-                }
-                else {
-                    let isSkillUsed = await db.UserSkill.findOne({
-                        where: { skillId: id }
-                    })
-                    if (isSkillUsed) {
-                        resolve({
-                            errCode: 3,
-                            errMessage: `Bạn không thể xóa thông tin này vì các dữ liệu khác liên quan`
-                        })
-                    }
-                    else {
-                        await db.Skill.destroy({
-                            where: { id: id }
-                        })
-                    }
-                    resolve({
-                        errCode: 0,
-                        errMessage: `Đã xóa thành công`
-                    })
-                }
-            }
-
-        } catch (error) {
-            reject(error.message)
-        }
-    })
-}
-
-let getAllSkillByJobCode = (categoryJobCode) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!categoryJobCode) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing required parameters !'
-                })
-            }
-            else {
-                let objectFilter = {
-                    include: [
-                        { model: db.Allcode, as: 'jobTypeSkillData', attributes: ['value', 'code'] }
-                    ],
-                    raw: true,
-                    nest: true
-                }
-                if (categoryJobCode !== 'getAll') {
-                    objectFilter.where = {
-                        [Op.and]: [
-                            db.Sequelize.where(db.sequelize.col('jobTypeSkillData.code'),{
-                                [Op.eq]: categoryJobCode
-                            }),
-                        ]
-                    }
-                }
-                let skills = await db.Skill.findAll(objectFilter)
-                resolve({
-                    errCode: 0,
-                    data: skills
-                })
-            }
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
-
-let getListSkill = (data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!data.limit || !data.offset) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing required parameters !'
-                })
-            }
-            else {
-                let objectFilter = {
-                    include: [
-                        { model: db.Allcode, as: 'jobTypeSkillData', attributes: ['value', 'code'] }
-                    ],
-                    limit: +data.limit,
-                    offset: +data.offset,
-                    raw: true,
-                    nest: true
-                }
-                if (data.search) {
-                    objectFilter.where = {
-                        ...objectFilter.where,
-                        name: {
-                            [Op.like]: `%${data.search}%`
-                        }
-                    }
-                }
-                if (data.categoryJobCode) {
-                    objectFilter.where = {
-                        ...objectFilter.where,
-                        [Op.and]: [
-                                db.Sequelize.where(db.sequelize.col('jobTypeSkillData.code'),{
-                                    [Op.eq]: data.categoryJobCode
-                                }),
-                            ]
-                    }
-                }
-                let skills = await db.Skill.findAndCountAll(objectFilter)
-                resolve({
-                    errCode: 0,
-                    data: skills.rows,
-                    count: skills.count
-                })
-            }
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
-
-let handleUpdateSkill = (data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!data.name || !data.id || !data.categoryJobCode) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing required parameters !'
-                })
-            } else {
-                let res = await db.Skill.findOne({
-                    where: {
-                        id: data.id
-                    },
-                    raw: false
-                })
-                if (res) {
-                    res.name = data.name
-                    res.categoryJobCode = data.categoryJobCode
-                    res = await res.save();
-                    if (res)
-                        resolve({
-                            errCode: 0,
-                            errMessage: 'Đã chỉnh sửa thành công'
-                        })
-                    else {
-                        resolve({
-                            errCode: 1,
-                            errMessage: 'Có lỗi trong quá trình chỉnh sửa'
-                        })
-                    }
-                }
-                else {
-                    resolve({
-                        errCode: 2,
-                        errMessage: 'Không tồn tại kĩ năng'
-                    })
-                }
-            }
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
-
-let getDetailSkillById = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            if (!id) {
-                resolve({
-                    errCode: 1,
-                    errMessage: 'Missing required parameters !'
-                })
-            } else {
-                let data = await db.Skill.findOne({
-                    where: { id: id },
-                    include: [
-                        {model: db.Allcode, as: 'jobTypeSkillData', attributes: ['value','code']}
-                    ],
-                    nest: true,
-                    raw: true
-                })
-                if (data)
-                    resolve({
-                        errCode: 0,
-                        data: data
-                    })
-                else {
-                    resolve({
-                        errCode: 1,
-                        errMessage: 'Không tìm thấy code'
-                    })
-                }
-            }
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
 module.exports = {
-    handleCreateNewAllCode: handleCreateNewAllCode,
-    getAllCodeService: getAllCodeService,
-    handleUpdateAllCode: handleUpdateAllCode,
-    getDetailAllcodeByCode: getDetailAllcodeByCode,
-    handleDeleteAllCode: handleDeleteAllCode,
     getListAllCodeService: getListAllCodeService,
     getListJobTypeAndCountPost: getListJobTypeAndCountPost,
     handleCreateNewSkill, handleDeleteSkill,
     getAllSkillByJobCode, getListSkill,
-    handleUpdateSkill, getDetailSkillById
+    handleUpdateSkill, getDetailSkillById,
+    handleCreateNewAllCode: handleCreateNewAllCode,
+    getAllCodeService: getAllCodeService,
+    handleUpdateAllCode: handleUpdateAllCode,
+    getDetailAllcodeByCode: getDetailAllcodeByCode,
+    handleDeleteAllCode: handleDeleteAllCode
 }
+
+   
